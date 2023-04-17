c118e0f3b1829f7e88c4f8042d71b781b657e738
Abigail Doyle <abigaildoyle3@gmail.com>
2023-04-16 19:29:11 -0700

    keyboard works?

--- src/pages/Songs.js
+++ src/pages/Songs.js
@@ -1,133 +1,190 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function Songs() {
+  useEffect(() => {
+    const keysArr = [...document.querySelectorAll(".key")];
 
+    const getKey = (event) => {
+      const parsedKey = event.key.toLowerCase().replace("\\", "\\\\");
+      console.log("parsedKey:", parsedKey);
+      const parsedCode = event.code.toLowerCase();
+      console.log("parsedCode:", parsedCode);
+      const element =
+        document.querySelector(`[data-key="${parsedCode}"]`) ||
+        document.querySelector(`[data-key="${parsedKey}"]`);
+      console.log("element:", element);
 
-  const keysArr = [...document.querySelectorAll(".key")];
-
-  const getKey = (event) => {
-    const parsedKey = event.key.toLowerCase().replace("\\", "\\\\");
-    console.log("parsedKey:", parsedKey)
-    const parsedCode = event.code.toLowerCase();
-    console.log("parsedCode:", parsedCode)
-    const element =
-      document.querySelector(`[data-key="${parsedCode}"]`) ||
-      document.querySelector(`[data-key="${parsedKey}"]`);
-      console.log("element:", element)
-
-    return element;
-
-  };
-  
-  document.addEventListener("keydown", (event) => {
-    const key = getKey(event);
-    
-    if (key) {
-      console.log("key:", key)
-      key.classList.add("active");
-    }
-  });
-  
-  document.addEventListener("keyup", (event) => {
-    const key = getKey(event);
-    if (key) {
-      console.log("key:", key)
-      key.classList.remove("active");
-    }
-  });
-  
-  document.addEventListener("mousedown", (event) => {
-    if (event.target.dataset.key) {
-      console.log("event.target.dataset.key:", event.target.dataset.key)
-      event.target.classList.add("active");
-    }
-  });
-  
-  document.addEventListener("mouseup", (event) => {
-    if (event.target.dataset.key) {
-      console.log("event.target.dataset.key:", event.target.dataset.key)
-      event.target.classList.remove("active");
-    }
-  });
-  
-  document.addEventListener("touchstart", (event) => {
-    if (event.target.dataset.key) {
-      console.log("event.target.dataset.key:", event.target.dataset.key)
-      event.target.classList.add("active");
-    }
-  });
-  
-  document.addEventListener("touchend", (event) => {
-    if (event.target.dataset.key) {
-      console.log("event.target.dataset.key:", event.target.dataset.key)
-      event.target.classList.remove("active");
-    }
-  });
-  
-  
-  const animate = (element) => {
-    const hueColor = Math.floor(Math.random() * (360 - 0 + 1)) + 0;
-    const color = `hsla(${hueColor}, 100%, 50%, 50%)`;
-    const textColor = `hsl(${hueColor}, 100%, 50%)`;
-    const textShadow = `0 0 0.80em ${color}, 0 0 1.60em ${color}, 0 0 4em ${color}`;
-    const boxShadow = `-3px 3px 4px ${color}, 3px -3px 4px ${color}, 3px 3px 4px ${color}, -3px -3px 4px ${color}, 0 0 10px ${color}`;
-  
-    const keyIndex = keysArr.indexOf(element);
-    const animatedKeysRight = keysArr.slice(keyIndex);
-    const animatedKeysLeft = keysArr.slice(0, keyIndex);
-  
-    const transitionHandler = (event) => {
-      event.target.style.boxShadow = "none";
-      event.target.style.color = null;
-      event.target.style.textShadow = "none";
-      event.target.removeEventListener("transitionend", transitionHandler);
+      return element;
     };
-  
-    animatedKeysRight.forEach((keyEl, i) => {
-      setTimeout(() => {
-        keyEl.addEventListener("transitionend", transitionHandler);
-        keyEl.style.boxShadow = boxShadow;
-        keyEl.style.color = textColor;
-        keyEl.style.textShadow = textShadow;
-      }, i * 35);
+
+    // keyboard events
+    const addActiveClassOnKeydown = (event) => {
+      const key = getKey(event);
+      if (key) {
+        key.classList.add("active");
+      }
+    };
+    document.addEventListener("keydown", addActiveClassOnKeydown);
+
+    const removeActiveClassOnKeyup = (event) => {
+      const key = getKey(event);
+      if (key) {
+        key.classList.remove("active");
+      }
+    };
+    document.addEventListener("keyup", removeActiveClassOnKeyup);
+
+
+    // mouseclick events
+    const addActiveClassOnMousedown = (event) => {
+      if (event.target.dataset.key) {
+        event.target.classList.add("active");
+      }
+    };
+    document.addEventListener("mousedown", addActiveClassOnMousedown);
+
+    const removeActiveClassOnMouseup = (event) => {
+      if (event.target.dataset.key) {
+        event.target.classList.remove("active");
+      }
+    };
+    document.addEventListener("mouseup", removeActiveClassOnMouseup);
+
+    // touchstart events
+    const addActiveClassOnTouchstart = (event) => {
+      if (event.target.dataset.key) {
+        event.target.classList.add("active");
+      }
+    };
+    document.addEventListener("mousedown", addActiveClassOnTouchstart);
+
+    const removeActiveClassOnTouchend = (event) => {
+      if (event.target.dataset.key) {
+        event.target.classList.remove("active");
+      }
+    };
+    document.addEventListener("mouseup", removeActiveClassOnTouchend);
+
+
+
+
+
+    // document.addEventListener("keydown", (event) => {
+    //   const key = getKey(event);
+
+    //   if (key) {
+    //     console.log("key:", key);
+    //     key.classList.add("active");
+    //   }
+    // });
+
+    // document.addEventListener("keyup", (event) => {
+    //   const key = getKey(event);
+    //   if (key) {
+    //     console.log("key:", key);
+    //     key.classList.remove("active");
+    //   }
+    // });
+
+    // document.addEventListener("mousedown", (event) => {
+    //   if (event.target.dataset.key) {
+    //     console.log("event.target.dataset.key:", event.target.dataset.key);
+    //     event.target.classList.add("active");
+    //   }
+    // });
+
+    // document.addEventListener("mouseup", (event) => {
+    //   if (event.target.dataset.key) {
+    //     console.log("event.target.dataset.key:", event.target.dataset.key);
+    //     event.target.classList.remove("active");
+    //   }
+    // });
+
+    // document.addEventListener("touchstart", (event) => {
+    //   if (event.target.dataset.key) {
+    //     console.log("event.target.dataset.key:", event.target.dataset.key);
+    //     event.target.classList.add("active");
+    //   }
+    // });
+
+    // document.addEventListener("touchend", (event) => {
+    //   if (event.target.dataset.key) {
+    //     console.log("event.target.dataset.key:", event.target.dataset.key);
+    //     event.target.classList.remove("active");
+    //   }
+    // });
+
+    const animate = (element) => {
+      const hueColor = Math.floor(Math.random() * (360 - 0 + 1)) + 0;
+      const color = `hsla(${hueColor}, 100%, 50%, 50%)`;
+      const textColor = `hsl(${hueColor}, 100%, 50%)`;
+      const textShadow = `0 0 0.80em ${color}, 0 0 1.60em ${color}, 0 0 4em ${color}`;
+      const boxShadow = `-3px 3px 4px ${color}, 3px -3px 4px ${color}, 3px 3px 4px ${color}, -3px -3px 4px ${color}, 0 0 10px ${color}`;
+
+      const keyIndex = keysArr.indexOf(element);
+      const animatedKeysRight = keysArr.slice(keyIndex);
+      const animatedKeysLeft = keysArr.slice(0, keyIndex);
+
+      const transitionHandler = (event) => {
+        event.target.style.boxShadow = "none";
+        event.target.style.color = null;
+        event.target.style.textShadow = "none";
+        event.target.removeEventListener("transitionend", transitionHandler);
+      };
+
+      animatedKeysRight.forEach((keyEl, i) => {
+        setTimeout(() => {
+          keyEl.addEventListener("transitionend", transitionHandler);
+          keyEl.style.boxShadow = boxShadow;
+          keyEl.style.color = textColor;
+          keyEl.style.textShadow = textShadow;
+        }, i * 35);
+      });
+
+      animatedKeysLeft.forEach((keyEl, j) => {
+        const i = animatedKeysLeft.length - j;
+        setTimeout(() => {
+          keyEl.addEventListener("transitionend", transitionHandler);
+          keyEl.style.boxShadow = boxShadow;
+          keyEl.style.color = textColor;
+          keyEl.style.textShadow = textShadow;
+        }, i * 35);
+      });
+    };
+
+    document.addEventListener("keydown", (event) => {
+      const key = getKey(event);
+
+      if (key) {
+        animate(key);
+        console.log("key:", key);
+      }
     });
-  
-    animatedKeysLeft.forEach((keyEl, j) => {
-      const i = animatedKeysLeft.length - j;
-      setTimeout(() => {
-        keyEl.addEventListener("transitionend", transitionHandler);
-        keyEl.style.boxShadow = boxShadow;
-        keyEl.style.color = textColor;
-        keyEl.style.textShadow = textShadow;
-      }, i * 35);
+
+    document.addEventListener("click", (event) => {
+      if (event.target.dataset.key) {
+        animate(event.target);
+        console.log("event.target:", event.target);
+      }
     });
-  };
-  
-  document.addEventListener("keydown", (event) => {
-    const key = getKey(event);
-  
-    if (key) {
+
+    window.addEventListener("load", () => {
+      const key = document.querySelector(`[data-key="enter"]`);
       animate(key);
-      console.log("key:", key)
-    }
-  });
-  
-  document.addEventListener("click", (event) => {
-    if (event.target.dataset.key) {
-      animate(event.target);
-      console.log("event.target:", event.target)
-    }
-  });
-  
-  window.addEventListener("load", () => {
-    const key = document.querySelector(`[data-key="enter"]`);
-    animate(key);
-    console.log("key:", key)
-  });
+      console.log("key:", key);
+    });
 
+    return () => {  // cleanup
+      document.removeEventListener("keydown", addActiveClassOnKeydown);
+      document.removeEventListener("keyup", removeActiveClassOnKeyup);
+      document.removeEventListener("mousedown", addActiveClassOnMousedown);
+      document.removeEventListener("mouseup", removeActiveClassOnMouseup);
+      document.removeEventListener("touchstart", addActiveClassOnTouchstart);
+      document.removeEventListener("touchend", removeActiveClassOnTouchend);
+    };
 
-  
-  
+  }, []);
+
   return (
     <>
       <div className="keyboardcontainer">
