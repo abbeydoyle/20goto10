4c111d46740dd34afb6a5b0073340f34869c8105
Abigail Doyle <abigaildoyle3@gmail.com>
2023-04-17 13:26:16 -0700

    mixer animations

--- src/pages/Songs.js
+++ src/pages/Songs.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Slider from "@mui/material/Slider";
 import Stack from "@mui/material/Stack";
 import Divider from "@mui/material/Divider";
@@ -6,142 +6,147 @@ import { BsFillVolumeMuteFill, BsFillVolumeUpFill } from "react-icons/bs";
 import { AiFillFastBackward, AiFillFastForward } from "react-icons/ai";
 
 export default function Songs() {
-  // useEffect(() => {
-  //   const keysArr = [...document.querySelectorAll(".key")];
+  useEffect(() => {
+    const keysArr = [...document.querySelectorAll(".mixbutton")];
 
-  //   const getKey = (event) => {
-  //     const parsedKey = event.key.toLowerCase().replace("\\", "\\\\");
-  //     console.log("parsedKey:", parsedKey);
-  //     const parsedCode = event.code.toLowerCase();
-  //     console.log("parsedCode:", parsedCode);
-  //     const element =
-  //       document.querySelector(`[data-key="${parsedCode}"]`) ||
-  //       document.querySelector(`[data-key="${parsedKey}"]`);
-  //     console.log("element:", element);
+    const getKey = (event) => {
+      const parsedKey = event.mixbutton.toLowerCase().replace("\\", "\\\\");
+      console.log("parsedKey:", parsedKey);
+      const parsedCode = event.code.toLowerCase();
+      console.log("parsedCode:", parsedCode);
+      const element =
+        document.querySelector(`[data-key="${parsedCode}"]`) ||
+        document.querySelector(`[data-key="${parsedKey}"]`);
+      console.log("element:", element);
 
-  //     return element;
-  //   };
+      return element;
+    };
 
-  //   // keyboard events
-  //   const addActiveClassOnKeydown = (event) => {
-  //     const key = getKey(event);
-  //     if (key) {
-  //       key.classList.add("active");
-  //     }
-  //   };
-  //   document.addEventListener("keydown", addActiveClassOnKeydown);
+    // keyboard events
+    const addActiveClassOnKeydown = (event) => {
+      const key = getKey(event);
+      if (key) {
+        key.classList.add("active");
+      }
+    };
+    document.addEventListener("keydown", addActiveClassOnKeydown);
 
-  //   const removeActiveClassOnKeyup = (event) => {
-  //     const key = getKey(event);
-  //     if (key) {
-  //       key.classList.remove("active");
-  //     }
-  //   };
-  //   document.addEventListener("keyup", removeActiveClassOnKeyup);
+    const removeActiveClassOnKeyup = (event) => {
+      const key = getKey(event);
+      if (key) {
+        key.classList.remove("active");
+      }
+    };
+    document.addEventListener("keyup", removeActiveClassOnKeyup);
 
-  //   // mouseclick events
-  //   const addActiveClassOnMousedown = (event) => {
-  //     if (event.target.dataset.key) {
-  //       event.target.classList.add("active");
-  //     }
-  //   };
-  //   document.addEventListener("mousedown", addActiveClassOnMousedown);
+    // mouseclick events
+    const addActiveClassOnMousedown = (event) => {
+      if (event.target.dataset.key) {
+        event.target.classList.add("active");
+      }
+    };
+    document.addEventListener("mousedown", addActiveClassOnMousedown);
 
-  //   const removeActiveClassOnMouseup = (event) => {
-  //     if (event.target.dataset.key) {
-  //       event.target.classList.remove("active");
-  //     }
-  //   };
-  //   document.addEventListener("mouseup", removeActiveClassOnMouseup);
+    const removeActiveClassOnMouseup = (event) => {
+      if (event.target.dataset.key) {
+        event.target.classList.remove("active");
+      }
+    };
+    document.addEventListener("mouseup", removeActiveClassOnMouseup);
 
-  //   // touchstart events
-  //   const addActiveClassOnTouchstart = (event) => {
-  //     if (event.target.dataset.key) {
-  //       event.target.classList.add("active");
-  //     }
-  //   };
-  //   document.addEventListener("mousedown", addActiveClassOnTouchstart);
+    // touchstart events
+    const addActiveClassOnTouchstart = (event) => {
+      if (event.target.dataset.key) {
+        event.target.classList.add("active");
+      }
+    };
+    document.addEventListener("mousedown", addActiveClassOnTouchstart);
 
-  //   const removeActiveClassOnTouchend = (event) => {
-  //     if (event.target.dataset.key) {
-  //       event.target.classList.remove("active");
-  //     }
-  //   };
-  //   document.addEventListener("mouseup", removeActiveClassOnTouchend);
+    const removeActiveClassOnTouchend = (event) => {
+      if (event.target.dataset.key) {
+        event.target.classList.remove("active");
+      }
+    };
+    document.addEventListener("mouseup", removeActiveClassOnTouchend);
 
-  //   const animate = (element) => {
-  //     const hueColor = Math.floor(Math.random() * (360 - 0 + 1)) + 0;
-  //     const color = `hsla(${hueColor}, 100%, 50%, 50%)`;
-  //     const textColor = `hsl(${hueColor}, 100%, 50%)`;
-  //     const textShadow = `0 0 0.80em ${color}, 0 0 1.60em ${color}, 0 0 4em ${color}`;
-  //     const boxShadow = `-3px 3px 4px ${color}, 3px -3px 4px ${color}, 3px 3px 4px ${color}, -3px -3px 4px ${color}, 0 0 10px ${color}`;
+    const animate = (element) => {
+      const hueColor = Math.floor(Math.random() * (360 - 0 + 1)) + 0;
+      const color = `hsla(${hueColor}, 100%, 50%, 50%)`;
+      const textColor = `hsl(${hueColor}, 100%, 50%)`;
+      const textShadow = `0 0 0.80em ${color}, 0 0 1.60em ${color}, 0 0 4em ${color}`;
+      const boxShadow = `-3px 3px 4px ${color}, 3px -3px 4px ${color}, 3px 3px 4px ${color}, -3px -3px 4px ${color}, 0 0 10px ${color}`;
 
-  //     const keyIndex = keysArr.indexOf(element);
-  //     const animatedKeysRight = keysArr.slice(keyIndex);
-  //     const animatedKeysLeft = keysArr.slice(0, keyIndex);
+      const keyIndex = keysArr.indexOf(element);
+      const animatedKeysRight = keysArr.slice(keyIndex);
+      const animatedKeysLeft = keysArr.slice(0, keyIndex);
 
-  //     const transitionHandler = (event) => {
-  //       event.target.style.boxShadow = "none";
-  //       event.target.style.color = null;
-  //       event.target.style.textShadow = "none";
-  //       event.target.removeEventListener("transitionend", transitionHandler);
-  //     };
+      const transitionHandler = (event) => {
+        event.target.style.boxShadow = "none";
+        event.target.style.color = null;
+        event.target.style.textShadow = "none";
+        event.target.removeEventListener("transitionend", transitionHandler);
+      };
 
-  //     animatedKeysRight.forEach((keyEl, i) => {
-  //       setTimeout(() => {
-  //         keyEl.addEventListener("transitionend", transitionHandler);
-  //         keyEl.style.boxShadow = boxShadow;
-  //         keyEl.style.color = textColor;
-  //         keyEl.style.textShadow = textShadow;
-  //       }, i * 35);
-  //     });
+      animatedKeysRight.forEach((keyEl, i) => {
+        setTimeout(() => {
+          keyEl.addEventListener("transitionend", transitionHandler);
+          keyEl.style.boxShadow = boxShadow;
+          keyEl.style.color = textColor;
+          keyEl.style.textShadow = textShadow;
+        }, i * 35);
+      });
 
-  //     animatedKeysLeft.forEach((keyEl, j) => {
-  //       const i = animatedKeysLeft.length - j;
-  //       setTimeout(() => {
-  //         keyEl.addEventListener("transitionend", transitionHandler);
-  //         keyEl.style.boxShadow = boxShadow;
-  //         keyEl.style.color = textColor;
-  //         keyEl.style.textShadow = textShadow;
-  //       }, i * 35);
-  //     });
-  //   };
+      animatedKeysLeft.forEach((keyEl, j) => {
+        const i = animatedKeysLeft.length - j;
+        setTimeout(() => {
+          keyEl.addEventListener("transitionend", transitionHandler);
+          keyEl.style.boxShadow = boxShadow;
+          keyEl.style.color = textColor;
+          keyEl.style.textShadow = textShadow;
+        }, i * 35);
+      });
+    };
 
-  //   document.addEventListener("keydown", (event) => {
-  //     const key = getKey(event);
+    document.addEventListener("keydown", (event) => {
+      const key = getKey(event);
 
-  //     if (key) {
-  //       animate(key);
-  //       console.log("key:", key);
-  //     }
-  //   });
+      if (key) {
+        animate(key);
+        console.log("key:", key);
+      }
+    });
 
-  //   document.addEventListener("click", (event) => {
-  //     if (event.target.dataset.key) {
-  //       animate(event.target);
-  //       console.log("event.target:", event.target);
-  //     }
-  //   });
+    document.addEventListener("click", (event) => {
+      if (event.target.dataset.key) {
+        animate(event.target);
+        console.log("event.target:", event.target);
+      }
+    });
 
-  //   window.addEventListener("load", () => {
-  //     const key = document.querySelector(`[data-key="enter"]`);
-  //     animate(key);
-  //     console.log("key:", key);
-  //   });
+    window.addEventListener("load", () => {
+      const key = document.querySelector(`[data-key="enter"]`);
+      animate(key);
+      console.log("key:", key);
+    });
 
-  //   return () => {  // cleanup
-  //     document.removeEventListener("keydown", addActiveClassOnKeydown);
-  //     document.removeEventListener("keyup", removeActiveClassOnKeyup);
-  //     document.removeEventListener("mousedown", addActiveClassOnMousedown);
-  //     document.removeEventListener("mouseup", removeActiveClassOnMouseup);
-  //     document.removeEventListener("touchstart", addActiveClassOnTouchstart);
-  //     document.removeEventListener("touchend", removeActiveClassOnTouchend);
-  //   };
+    return () => {  // cleanup
+      document.removeEventListener("keydown", addActiveClassOnKeydown);
+      document.removeEventListener("keyup", removeActiveClassOnKeyup);
+      document.removeEventListener("mousedown", addActiveClassOnMousedown);
+      document.removeEventListener("mouseup", removeActiveClassOnMouseup);
+      document.removeEventListener("touchstart", addActiveClassOnTouchstart);
+      document.removeEventListener("touchend", removeActiveClassOnTouchend);
+    };
 
-  // }, []);
+
+  }, []);
 
   function valuetext(value) {
     return `${value}%`;
+  }
+
+  const playAudio = () => {
+      
   }
 
   return (
@@ -150,7 +155,12 @@ export default function Songs() {
         <div className="mixer mt-[10%] grid grid-flow-col auto-cols-2 gap-2">
           <div className="min-w-[20rem]">
             <h1 className="pb-2 pt-2 text-xl">mixboard v20goto10</h1>
-            <Stack className="h-[75%] pt-2 pb-2 flex justify-evenly"  direction="row" spacing={2} divider={<Divider orientation="vertical" flexItem />}>
+            <Stack
+              className="h-[75%] pt-2 pb-2 flex justify-evenly"
+              direction="row"
+              spacing={2}
+              divider={<Divider orientation="vertical" flexItem />}
+            >
               <Stack justifyContent="center" alignItems="center">
                 <BsFillVolumeUpFill className="h-8 w-8 text-center" />
                 <Slider
@@ -256,60 +266,82 @@ export default function Songs() {
                 <AiFillFastBackward className="h-8 w-8 text-center" />
               </Stack>
             </Stack>
-            <Stack direction="row" justifyContent="center" alignItems="center" spacing={2} divider={<Divider orientation="vertical" flexItem />}>
-                  <div className="secondarymixbutton">button 1</div>
-                  <div className="secondarymixbutton">button 2</div>
-                  <div className="secondarymixbutton">button 3</div>
+            <Stack
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+              spacing={2}
+              divider={<Divider orientation="vertical" flexItem />}
+            >
+              <div className="secondarymixbutton">button 1</div>
+              <div className="secondarymixbutton">button 2</div>
+              <div className="secondarymixbutton">button 3</div>
             </Stack>
           </div>
           <div className="mixbuttoncontainer justify-center">
-            <div className="mixbutton" data-key="1">
-              song 1
+            <div className="mixbutton relative" data-key="n">
+              Nobinobi <br /> 伸び伸び
+              <div className="absolute bottom-1 right-1">n</div>
             </div>
-            <div className="mixbutton" data-key="2">
-              song 2
+            <div className="mixbutton relative" data-key="s">
+            Shitsuren <br /> 失恋
+              <div className="absolute bottom-1 right-1">s</div>
             </div>
-            <div className="mixbutton" data-key="3">
-              song 3
+            <div className="mixbutton relative" data-key="k">
+            Kikai <br /> 機械
+              <div className="absolute bottom-1 right-1">k</div>
             </div>
-            <div className="mixbutton" data-key="4">
-              song 4
+            <div className="mixbutton relative" data-key="p">
+            Keppaku <br /> 潔白
+              <div className="absolute bottom-1 right-1">p</div>
             </div>
-            <div className="mixbutton" data-key="5">
-              song 5
+            <div className="mixbutton relative" data-key="y">
+            Yoisho <br /> よいしょ
+              <div className="absolute bottom-1 right-1">y</div>
             </div>
-            <div className="mixbutton" data-key="6">
-              song 6
+            <div className="mixbutton relative" data-key="g">
+            Sutageiza <br /> スターゲイザー
+              <div className="absolute bottom-1 right-1">g</div>
             </div>
-            <div className="mixbutton" data-key="7">
-              song 7
+            <div className="mixbutton relative" data-key="a">
+            Kaikoshumi <br /> 懐古趣味
+              <div className="absolute bottom-1 right-1">a</div>
             </div>
-            <div className="mixbutton" data-key="8">
-              song 8
+            <div className="mixbutton relative" data-key="u">
+            Koukai <br /> 後悔
+              <div className="absolute bottom-1 right-1">u</div>
             </div>
-            <div className="mixbutton" ata-key="9">
-              song 9
+            <div className="mixbutton relative" data-key="i">
+            Koukai <br /> Instrumental <br />後悔
+              <div className="absolute bottom-1 right-1">i</div>
             </div>
-            <div className="mixbutton" data-key="10">
-              song 10
+            <div className="mixbutton relative" data-key="u">
+            Ukiyo <br /> 浮世
+              <div className="absolute bottom-1 right-1">u</div>
             </div>
-            <div className="mixbutton" data-key="11">
-              song 11
+            <div className="mixbutton relative" data-key="h">
+            Hitomebore <br /> 目惚れ
+              <div className="absolute bottom-1 right-1">h</div>
             </div>
-            <div className="mixbutton" data-key="12">
-              song 12
+            <div className="mixbutton relative" data-key="r" onClick={playAudio}>
+            Ryoko <br /> 旅行
+              <div className="absolute bottom-1 right-1">r</div>
             </div>
-            <div className="mixbutton" data-key="13">
-              song 13
+            <div className="mixbutton relative" data-key="o">
+            Kyusoku <br /> 休息
+              <div className="absolute bottom-1 right-1">o</div>
             </div>
-            <div className="mixbutton" data-key="14">
-              song 14
+            <div className="mixbutton relative" data-key="z">
+            Nozomi <br /> 希
+              <div className="absolute bottom-1 right-1">z</div>
             </div>
-            <div className="mixbutton" data-key="15">
-              song 15
+            <div className="mixbutton relative" data-key="e">
+              Unreleased <br /> 未発表
+              <div className="absolute bottom-1 right-1">e</div>
             </div>
-            <div className="mixbutton" data-key="16">
-              song 16
+            <div className="mixbutton relative" data-key="d">
+              Unreleased <br /> 未発表
+              <div className="absolute bottom-1 right-1">d</div>
             </div>
           </div>
         </div>
